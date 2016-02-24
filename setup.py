4a038f1e14e117351f6f835ff2a363de4da375f9
jgrande <jgrande@el-gas.es>
2016-02-24 07:51:24 +0100

    NEW v0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mailcertificado',
-    version='0.0.1-dev',
+    version='0.0.2',
     url='',
     author='Joan Manuel Grande',
     author_email='totaler@gmail.com',
